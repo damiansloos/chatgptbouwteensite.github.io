81678a2106f56a284c0161d64b3d6925a2077cb5
damiansloos <112393229+damiansloos@users.noreply.github.com>
2023-09-17 15:55:52 +0200

    Update submit_contact.php

--- submit_contact.php
+++ submit_contact.php
@@ -1,1 +1,29 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
 
+    // E-mailadres waar de e-mail naartoe wordt gestuurd
+    $to = "jouw-email@example.com";
+
+    // Onderwerp van de e-mail
+    $subject = "Nieuw contactformulierbericht van $name";
+
+    // Berichtinhoud
+    $email_message = "Naam: $name\n";
+    $email_message .= "E-mail: $email\n";
+    $email_message .= "Bericht:\n$message";
+
+    // E-mailheader instellen
+    $headers = "From: $email\n";
+    $headers .= "Reply-To: $email\n";
+
+    // E-mail verzenden
+    if (mail($to, $subject, $email_message, $headers)) {
+        echo "<p>Bedankt voor je bericht. We nemen zo spoedig mogelijk contact met je op.</p>";
+    } else {
+        echo "<p>Er is een probleem opgetreden bij het verzenden van je bericht. Probeer het later opnieuw.</p>";
+    }
+}
+?>
