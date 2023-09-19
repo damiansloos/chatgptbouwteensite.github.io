4746d422170fd5e7c7bf32d214e7f514711f61d2
damiansloos <112393229+damiansloos@users.noreply.github.com>
2023-09-19 11:08:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const contactForm = document.getElementById("contact-form");
+    const confirmationMessage = document.getElementById("confirmation-message");
 
     contactForm.addEventListener("submit", function (e) {
         e.preventDefault();
@@ -8,48 +9,21 @@ document.addEventListener("DOMContentLoaded", function () {
         const email = document.getElementById("email").value;
         const message = document.getElementById("message").value;
 
-        // Valideer de invoer hier (bijv. controleer of e-mail geldig is)
-
-        // Verzend de gegevens naar de server voor e-mailverzending
-        fetch("submit_contact.php", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                name: name,
-                email: email,
-                message: message,
-            }),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                // Hier kun je de serverreactie verwerken
-                if (data.success) {
-                    alert("Bedankt voor je bericht. We nemen zo spoedig mogelijk contact met je op.");
-                    contactForm.reset();
-                } else {
-                    alert("Er is een probleem opgetreden bij het verzenden van je bericht. Probeer het later opnieuw.");
-                }
-            })
-            .catch((error) => {
-                console.error("Fout bij het verzenden van het formulier: ", error);
-            });
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    const contactForm = document.getElementById("contact-form");
-    const confirmationMessage = document.getElementById("confirmation-message");
-
-    contactForm.addEventListener("submit", function (e) {
-        e.preventDefault();
-
         // Simuleer hier het verzenden van het formulier (bijv. met een AJAX-aanroep naar de server)
         // Voeg hier je e-mailverzending en verwerkingslogica toe
 
-        // Na succesvol verzenden, toon het bevestigingsbericht en verberg het formulier
+        // Hieronder wordt het bevestigingsbericht weergegeven en het formulier verborgen
         confirmationMessage.style.display = "block";
         contactForm.style.display = "none";
+
+        // Voeg hier je e-mailverzending en verwerkingslogica toe
+        // Deze code simuleert het verzenden van het formulier en het verwerken van de reactie
+        // Je moet deze code vervangen door echte e-mailverzending en serververwerking
+        setTimeout(function () {
+            // Hier simuleren we een succesvolle reactie van de server
+            // Je kunt deze setTimeout verwijderen en je echte e-mailverzending en verwerkingslogica hier plaatsen
+            alert("Bedankt voor je bericht. We nemen zo spoedig mogelijk contact met je op.");
+            contactForm.reset();
+        }, 1000); // Stel de time-out in op 1 seconde (1000 milliseconden)
     });
 });
