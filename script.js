03b821c32a57006153e486b22828db315b376976
damiansloos <112393229+damiansloos@users.noreply.github.com>
2023-09-21 20:37:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,8 @@
         .catch((error) => {
             console.error("Fout bij het verzenden van het formulier: ", error);
         });
+
+/* Dit is de javascript code, omdat de formulierverwerking via een andere, externe site gaat is die code hier niet meer nodig en had ik die code verwijderd.
+De code die hier staat is voor het generen van het bericht dat op de pagina van het contactformulier komt te staan zodra op de button wordt gedrukt.
+Als er op de button wordt gedrukt dan wordt de code geactiveerd. Verder wordt er gekeken of het bericht wordt verstuurd, zo ja dan krijg je data.success en dan krijg je het specifieke bericht dat het gelukt is.
+anders krijg je een bericht met de melding dat er een probleem is met het versturen en dan het later opnieuw geprobeerd moet worden. Ook zit er een controle op .catch((error) deze geeft aan bij het inspecteren dat er een fout is*/
